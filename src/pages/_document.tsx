d12efc2c3f060d50559f1eff9038c9d96df9f133
amirhosseinmousavi <a.hossainmousavi@gmail.com>
2024-03-11 18:57:58 +0330

    fix: solved problem for phone

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="fa" dir="rtl" className="scroll-smooth">
+    <Html lang="fa" dir="rtl" className="scroll-smooth overflow-x-hidden">
       <Head />
       <body>
         <Main />
